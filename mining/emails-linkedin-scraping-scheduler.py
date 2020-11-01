ce346715eb57451a86daf684a663067211827e83
sergii.volodko <svolodko@8thlight.com>
2020-11-01 22:08:35 +0000

    Mining: add dynamic schedulers number of run limit

--- mining/emails-linkedin-scraping-scheduler.py
+++ mining/emails-linkedin-scraping-scheduler.py
@@ -5,10 +5,32 @@ import sys
 from tqdm import tqdm
 from random import uniform
 import time
+import math
+from BookmarkRepo import BookmarkRepo
+
+root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+DATA_FOLDER = os.path.join(root_folder, "data")
 
 def load_config():
     with open(os.path.join(os.path.dirname(__file__), 'network_mining_config.json')) as f:
         return json.load(f)
+
+def get_number_of_runs(config):
+    ### Logic
+    # total = 500
+    # bookmark = 479
+    # batch_size = 20
+    # number_of_runs = 10
+    # result -> 2
+    number_of_runs = config["scheduler_number_of_scraper_runs"]
+    total_contacts = config["crawler_my_contacts_limit"]
+    scraper_batch_size = config["scraper_batch_size"]
+    bookmark_file = os.path.join(DATA_FOLDER, config["scraper_bookmark_file_name"])
+    profiles_bookmark = BookmarkRepo(bookmark_file).load_bookmark()
+    remaining_profiles = total_contacts - profiles_bookmark
+    if (number_of_runs * scraper_batch_size > remaining_profiles):
+        number_of_runs = math.ceil(remaining_profiles / scraper_batch_size)
+    return number_of_runs
 
 if __name__ == "__main__":
     print("\n\n----- Scheduler script started -----\n")
@@ -19,9 +41,10 @@ if __name__ == "__main__":
     print("Beginning the schedule...\n")
 
     config = load_config()
-    number_of_runs = config["scheduler_number_of_scraper_runs"]
-    delay_between_runs = config["scheduler_seconds_between_scraper_runs"]
+
     script_path = os.path.join(os.path.dirname(__file__), 'emails-linkedin-scraping-pipeline.py')
+    delay_between_runs = config["scheduler_seconds_between_scraper_runs"]
+    number_of_runs = get_number_of_runs(config)
 
     for i in tqdm(range(0, number_of_runs)):
         print("===================>> Starting run #{}".format(i + 1))
